6fb1c6f11eff9ce59a5a452443767aed4b173056
kang4742 <jiewon4742@gmail.com>
2020-11-21 19:49:04 +0900

    The 2nd Commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello, world form GIT!");
         System.out.println("After 1st Commit");
+        System.out.println("After 2nd Commit");
     }
 }
